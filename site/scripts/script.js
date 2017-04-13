2797140cf341c265fafbb9759be85b54c7bba109
p-noriega <pnoriega@est.frba.utn.edu.ar>
2017-04-13 15:34:22 -0300

    Subida de la página web con las pantallas de la entrega 1.

---
+++ site/scripts/script.js
@@ -0,0 +1,31 @@
+function toggleCuentaPrompt() {
+    $(".popup-bg").fadeToggle();
+}
+
+function toggleManualEntryPrompt() {
+    toggleCuentaPrompt();
+    $(".manual-entry").fadeToggle();
+}
+
+function toggleFileUploadPrompt() {
+    toggleCuentaPrompt();
+    $(".file-upload").fadeToggle();
+}
+
+function toggleFileUploadSuccess() {
+    $("#uploadSuccess").fadeToggle();
+}
+
+function toggleRegistrySuccess() {
+    $("#registrySuccess").fadeToggle();
+}
+
+function uploadSuccess() {
+  toggleFileUploadPrompt();
+  toggleFileUploadSuccess();
+}
+
+function registrySuccess() {
+  toggleManualEntryPrompt();
+  toggleRegistrySuccess();
+}
